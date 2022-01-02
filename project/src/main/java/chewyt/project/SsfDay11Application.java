b11a40f51b843cdb35ddb56e71e2c67b0987c610
chewyt <yeetian.chew@gmail.com>
2022-01-02 16:17:02 +0800

    first commit

---
+++ project/src/main/java/chewyt/project/SsfDay11Application.java
@@ -0,0 +1,31 @@
+package chewyt.project;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfDay11Application {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(SsfDay11Application.class, args);
+		SpringApplication app = new SpringApplication(SsfDay11Application.class);
+
+		String port = "8080";
+		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+		if (cliOpts.containsOption("port")) {
+			port = cliOpts.getOptionValues("port").get(0);
+		}
+		app.setDefaultProperties(
+			Collections.singletonMap("server.port",port)
+		);
+		System.out.printf("Application started on port %s\n", port);
+		app.run(args);
+
+	}
+
+}
