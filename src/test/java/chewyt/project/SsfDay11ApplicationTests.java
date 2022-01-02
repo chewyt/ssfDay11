3a8b020b98626d9d50e70da61ab4de7f72f28e47
chewyt <yeetian.chew@gmail.com>
2022-01-02 17:10:02 +0800

    first commit

---
+++ src/test/java/chewyt/project/SsfDay11ApplicationTests.java
@@ -0,0 +1,13 @@
+package chewyt.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfDay11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
